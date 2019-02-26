3fb7227312875e5dc25dfe7b8d85c91661db6cee
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 20:09:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  counter = 0
+  caps_lock = []
+  first_names = []
+  
+  while counter < array.length
+    yield caps_lock[counter] = array[counter].upcase
+    yield first_names[counter] = array[counter].split.first
+    counter = counter + 1
+  end
+  
+  caps_lock
+  first_names
+end
